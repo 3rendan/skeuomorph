17b12191167859dba4263935b2c4e5b961e733d9
3rendan <brendan.ryan@pm.me>
2020-04-28 06:22:10 -0400

    switched to non-filtering dcKeys to ensure that new item entry works.

--- src/views/NewItems.js
+++ src/views/NewItems.js
@@ -54,13 +54,19 @@ class NewItems extends Component {
         <div className="contact-clean">
         <form onSubmit={this.handleSubmit}>
           <h1> New Item </h1>
-          { dcKeys.map(key => {
-            return (
-              <Fields value={key}) />
-            )}
-          <div className="form-group">
-              <input type="submit" className="submit btn btn-warning" />
-          </div>
+          { dcKeys.map((key, i) =>{
+           return(
+             <div key={key} className="form-group">
+             <label htmlFor={key} >{key}: </label>
+             <input
+               type="text"
+               id={key}
+               placeholder={ key }
+               value={this.state.formInputs.key}
+               onChange={this.handleChange}
+             />
+             </div>
+         )}) }
         </form>
       </div>
     </div>
